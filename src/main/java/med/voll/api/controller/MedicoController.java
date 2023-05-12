9f7e36c33eed3f9190809deccd952f66f6049f48
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-05-12 00:01:51 -0300

    Enviando e recebendo dados

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 
+import med.voll.api.medico.DadosCadastroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping
-    public void cadastrar (@RequestBody String json){
+    public void cadastrar (@RequestBody DadosCadastroMedico dados){
 
-        System.out.println(json);
+        System.out.println(dados);
 
     }
 
--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,2 +1,8 @@
-package med.voll.api.medico;public record DadosCadastroMedico() {
+package med.voll.api.medico;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+
+
 }
