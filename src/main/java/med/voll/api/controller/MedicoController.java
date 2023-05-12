b021da8557437a89804674a7b3bd89e7b98e9bfd
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-05-11 23:34:26 -0300

    Recebendo dados na API

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,6 +2,7 @@ package med.voll.api.controller;
 
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping
-    public void cadastrar (){
+    public void cadastrar (@RequestBody String json){
+
+        System.out.println(json);
 
     }
 
