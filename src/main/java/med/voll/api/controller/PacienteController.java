a592e9c168559c820f1c65f8e3ab8c5872fbd2bb
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-05-13 10:56:13 -0300

    Editando estrutura pacientes

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,2 +1,20 @@
-package med.voll.api.controller;public class PacienteController {
+package med.voll.api.controller;
+
+import med.voll.api.paciente.DadosCadastroPaciente;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping
+public class PacienteController {
+
+    @PostMapping("pacientes")
+    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
+        System.out.println("dados recebido: " +dados);
+    }
+
+
 }
--- src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -1,2 +1,7 @@
-package med.voll.api.paciente;public record DadosCadastroPaciente() {
+package med.voll.api.paciente;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroPaciente(String nome, String email, String telefone,String cpf, DadosEndereco endereco) {
+
 }
