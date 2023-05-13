8159046a01d72e329badb3c357fc81b0997a80fc
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-05-13 19:18:34 -0300

    editand  pom.xml

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,20 +1,2 @@
-package med.voll.api.controller;
-
-import med.voll.api.paciente.DadosCadastroPaciente;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-@RestController
-@RequestMapping
-public class PacienteController {
-
-    @PostMapping("pacientes")
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println("dados recebido: " +dados);
-    }
-
-
+package med.voll.api.controller;public class PacienteController {
 }
