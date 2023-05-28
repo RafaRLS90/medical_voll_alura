c8f6616d672f21266592a7bbd0455276292030ac
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-05-28 10:22:35 -0300

    editando e criando conexão com o banco de dados

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,2 +1,30 @@
-package med.voll.api.medico;public class Medico {
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter//gera os métodos getters
+@NoArgsConstructor//constrói os métodos default, sem argumentos
+@AllArgsConstructor//Construtor que recebe todos os campos
+@EqualsAndHashCode(of = "id")//gera o equalshashcode, encima do id
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
 }
