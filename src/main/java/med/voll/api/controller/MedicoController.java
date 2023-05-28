a382d5cebd5064f941b9ea13db8cb90024407b70
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-05-28 17:35:56 -0300

    ferramentas de migrações

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,6 +2,10 @@ package med.voll.api.controller;
 
 
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
+    @Transactional//transação ativa com o banco de dados
     public void cadastrar (@RequestBody DadosCadastroMedico dados){
 
-        System.out.println(dados);
+        repository.save(new Medico(dados));
 
     }
 
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -27,4 +27,13 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    public Medico(DadosCadastroMedico dados) {
+        //atribuição dos atributos
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+
+    }
 }
