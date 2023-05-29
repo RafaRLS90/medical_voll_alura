88ef218de41016afca6538ff1947baca2de03dcc
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-05-28 21:02:15 -0300

    dados, repository pacientes

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -1,2 +1,44 @@
-package med.voll.api.paciente;public class Paciente {
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter//gera os métodos getters
+@NoArgsConstructor//constrói os métodos default, sem argumentos
+@AllArgsConstructor//Construtor que recebe todos os campos
+@EqualsAndHashCode(of = "id")//gera o equalshashcode, encima do id
+
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    private String cpf;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(DadosCadastroPaciente dados) {
+        //atribuição dos atributos
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.cpf = dados.cpf();
+        this.endereco = new Endereco(dados.endereco());
+
+    }
+
 }
