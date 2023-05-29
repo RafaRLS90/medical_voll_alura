ef6c2b391b65bb80c16c0541f8cad6294572060b
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-05-28 22:44:57 -0300

    comando delete

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -37,7 +37,11 @@ public class MedicoController {
     public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+    }
 
-
+    @DeleteMapping("/{id}")
+    @Transactional
+        public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
